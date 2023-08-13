3fe145da904567c33e361e26c31d08399992904d
LireEruel <jsh001505@gmail.com>
2023-08-13 16:57:28 +0900

    Default Header 추가

--- components.d.ts
+++ components.d.ts
@@ -9,7 +9,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     AButton: typeof import('ant-design-vue/es')['Button']
     AInput: typeof import('ant-design-vue/es')['Input']
-    ASpace: typeof import('ant-design-vue/es')['Space']
     DefaultHeader: typeof import('./src/components/layouts/DefaultHeader.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
--- src/components/layouts/DefaultHeader.vue
+++ src/components/layouts/DefaultHeader.vue
@@ -1,27 +1,45 @@
 <template>
   <header id="defaultHeader">
-    <div class="header-left">
+    <div class="header-content">
       <label>{{ name }}</label>
-      <book-two-tone />
+      <div class="icon-button">
+        <book-two-tone />
+      </div>
     </div>
-    <div></div>
+    <div class="header-content">
+      <div class="icon-button">
+        <img src="@/assets/img/icons/user.svg" alt="participants info icon" />
+      </div>
+      <div class="icon-button">
+        <setting-two-tone />
+      </div>
+    </div>
   </header>
 </template>
 
 <script lang="ts" setup>
 import { userStore } from '@/stores/user'
-import { BookTwoTone } from '@ant-design/icons-vue'
-const { name, setName } = userStore()
+import { BookTwoTone, SettingTwoTone } from '@ant-design/icons-vue'
+const { name } = userStore()
 </script>
 
 <style lang="scss">
+@import '@/assets/styles/iconButton.scss';
+
 #defaultHeader {
   position: fixed;
   height: 5rem;
   display: flex;
-  justify-content: center;
-  align-items: space-between;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
+  padding: 1% 5%;
   font-size: 2rem;
+
+  .header-content {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+  }
 }
 </style>

227cfff9aa843d5086f4260a013231ae4c0db72a
LireEruel <jsh001505@gmail.com>
2023-08-16 13:48:38 +0900

    interface와 마굿간 이미지 추가

---
+++ src/assets/rooms.ts
@@ -0,0 +1,9 @@
+import { chatroom } from "@/types/chatroom";
+
+export const room_list:chatroom[] = [
+    {
+        chatroomId: 1,
+        chatroomName : '마굿간',
+        background_img_url : '@/assets/img/stall.png'
+    }
+]
---
+++ src/types/chatroom.ts
@@ -0,0 +1,6 @@
+export interface chatroom {
+    chatroomId: number
+    chatroomName : string
+    background_img_url : string
+  }
+  

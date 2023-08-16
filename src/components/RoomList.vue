0908ba3bc2d70e7b0706d49e8377441da9095e0c
LireEruel <jsh001505@gmail.com>
2023-08-16 22:03:55 +0900

    chat-room으로 이동까지

--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -1,36 +1,35 @@
 <template>
   <div class="roomList">
-    <div v-for="(room, index) in roomList" :key="index" class="room">{{ room }}</div>
+    <div
+      v-for="room in chatRoomInfoList"
+      :key="room.chatRoomId"
+      class="room"
+      @click="() => onClickRoom(room)"
+    >
+      {{ room.chatRoomName }}
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const roomList = [
-  '창고',
-  '교실',
-  '분수대',
-  '마법관',
-  '별빛 관찰대',
-  '시계탑 중층',
-  '시장 뒷골목',
-  '고블린의 동굴',
-  '도서관',
-  '연구실',
-  '사무실',
-  '왕실 정원',
-  '대성당',
-  '박물관',
-  '오페라 하우스',
-  '지하실',
-  '해변가',
-  '고요한 호수',
-  '수련의 숲',
-  '폐가',
-  '산 정상',
-  '시계탑 상층',
-  '마법사의 방',
-  '용의 동굴'
-]
+import { chatRoomInfoList } from '@/assets/chatRooms'
+import { type ChatRoomInfo } from '@/types/chat'
+import { useRouter } from 'vue-router'
+import Swal from 'sweetalert2'
+
+const router = useRouter()
+
+const onClickRoom = (room: ChatRoomInfo) => {
+  Swal.fire({
+    title: room.chatRoomName + '으로 이동할까요?',
+    showCancelButton: true,
+    heightAuto: false
+  }).then((result) => {
+    if (result.isConfirmed) {
+      router.push('/chat-room')
+    }
+  })
+}
 </script>
 
 <style lang="scss">
